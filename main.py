7dc0d8bb56156dbce2b17af73971204f04ab4075
Lagicrus <Lagicrus@users.noreply.github.com>
2018-02-22 09:58:12 +0000

    main

---
+++ main.py
@@ -0,0 +1,88 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+ids_to_check = []
+id_homepage = {}
+id_url_pages = {}
+id_total_stats = {}
+banned_links = ["w3.org", "google.co"]
+check_loop = True
+
+TAG_LIST = ["br", "font", "center"]
+
+def homepage_grab(user_id):
+    w = requests.get("http://{}.web1.rdfx.org".format(user_id), timeout=30)
+    id_homepage[user_id] = {"content": w.content, "url": w.url}
+    print("Done {} {}".format(user_id, w.status_code))
+
+
+def doctype_checker(content, user_id, url):
+    if "<!DOCTYPE>" in content.decode("utf-8"):
+        return 1
+        print("<!DOCTYPE> used in {} {}".format(user_id, repr(url)))
+    elif "<!DOCTYPE html>" in content.decode("utf-8"):
+        return 1
+        print("<!DOCTYPE html> used in {} {}".format(user_id, repr(url)))
+    else:
+        return 0
+
+
+def tag_usage_checker(soup, user_id, tag, url):
+    amount = soup.find_all(tag)
+    return len(amount)
+    print("{} uses of <{}> in {} by {}".format(len(amount), tag, url, user_id))
+
+
+def recursive_page_finder(soup, user_id):
+    id_url_pages[user_id] = []
+    links = soup.find_all("a")
+    hyperlinks = [link["href"] for link in links]
+    for link in hyperlinks:
+        if not any(disallowed_links in link for disallowed_links in banned_links):
+            if link.startswith("#"):
+                print("Didn't add {}".format("http://{}.web1.rdfx.org/{}".format(user_id, link)))
+            elif "http://{}.web1.rdfx.org/{}".format(user_id, link) not in id_url_pages[user_id]:
+                test = requests.get("http://{}.web1.rdfx.org/{}".format(user_id, link))
+                if test.status_code == 200:
+                    id_url_pages[user_id].append("http://{}.web1.rdfx.org/{}".format(user_id, link))
+                    print("Added {}".format("http://{}.web1.rdfx.org/{}".format(user_id, link)))
+                else:
+                    print("Didn't add {}".format("http://{}.web1.rdfx.org/{}".format(user_id, link)))
+
+
+while check_loop:
+    raw_ids = input("What ID's do you want to check? Please input in a space separated list: ")
+    id_list = raw_ids.split(" ")
+    if len(id_list) > 10:
+        print("At MAX you should have 10 ID's. Please try again.")
+    else:
+        ids_to_check = id_list
+        print(", ".join(ids_to_check))
+        check = input("Is this list correct for the IDs to check? ").upper()
+        if check.startswith("Y"):
+            check_loop = False
+
+for user_id in ids_to_check:
+    homepage_grab(user_id)
+
+for user_id in ids_to_check:
+    soup = BeautifulSoup(id_homepage[user_id]["content"], "html.parser")
+    recursive_page_finder(soup, user_id)
+
+print("\n")
+
+for user_id in ids_to_check:
+    id_total_stats[user_id] = {"page_amount": len(id_url_pages[user_id]), "doctype": 0, "tag": {}}
+    for tag in TAG_LIST:
+        id_total_stats[user_id]["tag"][tag] = 0
+
+for user_id in ids_to_check:
+    for link in id_url_pages[user_id]:
+        content = requests.get(link).content
+        id_total_stats[user_id]["doctype"] += doctype_checker(content, user_id, link)
+        soup = BeautifulSoup(content, "html.parser")
+        for tag in TAG_LIST:
+            id_total_stats[user_id]["tag"][tag] += tag_usage_checker(soup, user_id, tag, link)
+
+pprint(id_total_stats)
